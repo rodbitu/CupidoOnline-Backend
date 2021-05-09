ebf4e40690c20e2495c9a66f21e16f4d83acbe2b
Rodrigo Bitu <digomoraes0@gmail.com>
2021-05-09 11:12:48 -0300

    feat: Adding CORS to serverless

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -2,7 +2,6 @@ export default function handler(lambda) {
     return async function (event, context) {
       let body, statusCode;
       try {
-        // Run the Lambda
         body = await lambda(event, context);
         statusCode = 200;
       } catch (e) {
@@ -10,10 +9,13 @@ export default function handler(lambda) {
         body = { error: e.message };
         statusCode = 500;
       }
-      // Return HTTP response
       return {
         statusCode,
         body: JSON.stringify(body),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
     };
   }
